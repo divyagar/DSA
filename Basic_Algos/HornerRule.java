e660c49356dfb97b2a76f66a25ab7a34d8cea384
Divya Garg <divyagarg2601@gmail.com>
2020-10-06 16:29:15 +0530

    added horner's rule

---
+++ Basic_Algos/HornerRule.java
@@ -0,0 +1,26 @@
+package Algos;
+import java.util.*;
+public class HornerRule {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0; i<n; i++)
+			arr[i] = sc.nextInt();
+		
+		int x = sc.nextInt();
+		
+		int ans = arr[0];
+		int i = 1;
+		while(i < n){
+			ans *= x;
+			ans += arr[i];
+			i++;
+		}
+		
+		System.out.println(ans);
+		
+		
+		
+	}
+}
